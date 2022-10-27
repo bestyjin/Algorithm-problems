2b380da86a81bb52bbabf879000f206355fc2f9b
yjkim <71006256+bestyjin@users.noreply.github.com>
2022-10-27 11:18:43 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,10 @@
+def solution(brown, yellow):
+    total = brown + yellow
+    for b in range(1,total+1):
+        if total%b == 0:
+            a = total//b
+            if a<=b:
+                if 2*a+2*b -4 == brown:
+                    return[b,a]
+    
+    
