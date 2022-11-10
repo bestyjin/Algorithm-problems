0d6aa2cc1ce18f26ac02e6a27530c04a154f1f36
yjkim <71006256+bestyjin@users.noreply.github.com>
2022-11-10 22:00:04 +0900

    [level 2] Title: 구명보트, Time: 7.43 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -1,13 +1,12 @@
 def solution(people, limit):
-    answer = 0
+    answer=0
     people.sort()
-    start = 0
-    end = len(people)-1
-    while start <= end:
-        if people[start]+people[end] <= limit:
-            start+=1
-        end-=1
-        answer+=1
-        
-    return answer
+    a=0
+    b=len(people)-1
+    while a<b:
+        if people[a]+people[b] <= limit:
+            a+=1
+            answer+=1
+        b-=1
+    return len(people)-answer
             
