0577dcd8db2da80df064f2eb7aeee39e804810f3
yjkim <71006256+bestyjin@users.noreply.github.com>
2022-11-17 09:09:09 +0900

    [level 2] Title: 올바른 괄호, Time: 12.99 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.py
@@ -0,0 +1,18 @@
+def solution(s):
+    stack=[]
+    for i in s:
+        if len(stack)==0:
+            if i == '(':
+                stack.append(i)
+            else:
+                return False
+        else:
+            if i == '(':
+                stack.append(i)
+            else:
+                if stack[-1] == '(':
+                    stack.pop()
+    if len(stack)==0:
+        return True
+    else:
+        return False
