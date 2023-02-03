321faf8ab32bef2a923f68aa6293e12f989db5f1
yjkim <71006256+bestyjin@users.noreply.github.com>
2023-02-03 10:54:44 +0900

    [level 2] Title: 프린터, Time: 0.55 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42587. 프린터/프린터.js
+++ 프로그래머스/lv2/42587. 프린터/프린터.js
@@ -1,10 +1,9 @@
 class Node {
-    constructor(value){
+    constructor(value) {
         this.value = value;
         this.next = null;
     }
 }
-
 class Queue {
     constructor() {
         this.head = null;
@@ -12,14 +11,14 @@ class Queue {
     }
     enqueue(newValue){
         const newNode = new Node(newValue);
-        if(this.head === null) {
+        if (this.head === null){
             this.head = this.tail = newNode;
         } else {
             this.tail.next = newNode;
             this.tail = newNode;
         }
     }
-    dequeue() {
+    dequeue(){
         const value = this.head.value;
         this.head = this.head.next;
         return value;
@@ -30,22 +29,23 @@ class Queue {
 }
 
 function solution(priorities, location) {
-    const queue = new Queue();
-    for(let i = 0; i<priorities.length;i++){
+    let queue = new Queue();
+    for (let i = 0; i<priorities.length; i+=1){
         queue.enqueue([priorities[i],i]);
     }
     priorities.sort((a,b)=>b-a);
     let count = 0;
     while(true){
-        const currentValue = queue.peek();
-        if(currentValue[0] < priorities[count]) {
+        const currValue = queue.peek();
+        if(currValue[0] < priorities[count]){
             queue.enqueue(queue.dequeue());
         } else {
             const value = queue.dequeue();
-            count += 1;
-            if(value[1] === location){
+            count+=1;
+            if(value[1] === location) {
                 return count;
             }
         }
     }
+    
 }
