5f1966ceb115a6dddd2152b07435673b8a6b7025
yjkim <71006256+bestyjin@users.noreply.github.com>
2023-02-12 21:55:51 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,21 @@
+function solution(num) {
+    let count = 0;
+    if (num === 1){
+        return 0;
+    } else {
+        while(num != 1){
+            if (num % 2 == 0){
+                num = num/2;
+                count += 1;
+            } else {
+                num = num * 3 + 1;
+                count += 1;
+            }
+            if (count === 501){
+                return -1;
+            }
+        }
+    }
+    return count;
+    
+}
